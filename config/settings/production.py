76d71b2dc9629c99438d0097d97de4e52cf4e877
birgirthh <birgir.thor.hardarson@ruv.is>
2026-03-23 20:22:11 +0100

    Fix production settings for Heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,34 +2,39 @@ from .base import *
 import os
 import dj_database_url
 
-DEBUG = False
-
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-
-try:
-    from .local import *
-except ImportError:
-    pass
-
-
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    # ...
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    ".herokuapp.com,kyrauga.herokuapp.com"
+).split(",")
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    "CSRF_TRUSTED_ORIGINS",
+    "https://kyrauga.herokuapp.com"
+).split(",")
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Keep all middleware from base.py, just insert WhiteNoise after SecurityMiddleware
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+# Use hashed static files in production
+STORAGES["staticfiles"]["BACKEND"] = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 if "DATABASE_URL" in os.environ:
     DATABASES["default"] = dj_database_url.config(
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True,
     )
+
+try:
+    from .local import *
+except ImportError:
+    pass
