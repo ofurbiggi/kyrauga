dc2654cb20bacc4cdb398e364d8d7591dce5f5d1
birgirthh <birgir.thor.hardarson@ruv.is>
2026-06-24 13:31:44 +0200

    Configure runtime and development tooling

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,15 +8,21 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    ".herokuapp.com,kyrauga.herokuapp.com"
+    "kyrauga.is,www.kyrauga.is,kyrauga.herokuapp.com,.herokuapp.com",
 ).split(",")
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS",
-    "https://kyrauga.herokuapp.com"
+    "https://kyrauga.is,https://www.kyrauga.is,https://kyrauga.herokuapp.com",
 ).split(",")
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True") == "True"
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = int(os.getenv("SECURE_HSTS_SECONDS", "31536000"))
+SECURE_HSTS_INCLUDE_SUBDOMAINS = os.getenv("SECURE_HSTS_INCLUDE_SUBDOMAINS", "False") == "True"
+SECURE_HSTS_PRELOAD = os.getenv("SECURE_HSTS_PRELOAD", "False") == "True"
 
 # Keep all middleware from base.py, just insert WhiteNoise after SecurityMiddleware
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
